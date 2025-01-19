2e0a495df8eb031d74fc6eb874f368b51bbf0b7e
Josh Robbs <josh@joshrobbs.com>
2025-01-19 10:06:37 -0500

    More testing

--- helpers/plugin-updates.php
+++ helpers/plugin-updates.php
@@ -19,18 +19,18 @@ function my_plugin_check_for_updates( $transient ) {
 
 	// Get current plugin version and URI dynamically.
 	$plugin_data     = get_plugin_data( __FILE__ ); // __FILE__ is the main plugin file.
-	$current_version = $plugin_data['Version'];
+	$current_version = $plugin_data['Version'] ?? '0.0.1';
 	$plugin_uri      = $plugin_data['PluginURI'];
-
-	if ( is_null( $current_version ) ) {
-		$current_version = '0.0.1';
-	}
 
 	$response = wp_remote_get( 'https://raw.githubusercontent.com/jwrobbs/jwr-dev-plugin/main/plugin-update.json' );
 
 	if ( is_wp_error( $response ) ) {
 		return $transient;
 	}
+
+	error_log( 'Plugin data: ' . print_r( $plugin_data, true ) );
+	error_log( 'Current version: ' . $current_version );
+	error_log( 'Reponse: ' . wp_remote_retrieve_body( $response ) );
 
 	$update_data = json_decode( wp_remote_retrieve_body( $response ), true );
 	if ( version_compare( $update_data['version'], $current_version, '>' ) ) {
@@ -44,4 +44,9 @@ function my_plugin_check_for_updates( $transient ) {
 
 	return $transient;
 }
-	add_filter( 'site_transient_update_plugins', __NAMESPACE__ . '\my_plugin_check_for_updates' );
+add_action(
+	'plugin_loaded',
+	function () {
+		add_filter( 'site_transient_update_plugins', __NAMESPACE__ . '\my_plugin_check_for_updates' );
+	}
+);
