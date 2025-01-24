f4ccff72ea04e7cf6ee07536d58e72fd44b57f41
Josh Robbs <josh@joshrobbs.com>
2025-01-24 17:51:46 -0500

    Bump version to 0.2.0 and update plugin path and URL constants

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  * Plugin Name:       JWR Dev Plugin
  * Plugin URI:        https://github.com/jwrobbs/jwr-dev-plugin
  * Description:       Adds scripts and widgets for devs.
- * Version:           0.1.4
+ * Version:           0.2.0
  * Author:            Josh Robbs <josh@joshrobbs.com>
  * Author URI:        https://joshrobbs.com
  * Text Domain:       jwr
@@ -16,18 +16,27 @@
 
 namespace JWR\DevPlugin;
 
-use JWR_Dev_Plugin\Admin_Page;
+use JWR_Dev_Plugin\Classes\Admin_Page;
+use JWR_Dev_Plugin\Classes\AME_Styler;
 
 defined( 'ABSPATH' ) || exit;
 
-if ( ! defined( 'JWR_DEV_PLUGIN' ) ) {
-	define( 'JWR_DEV_PLUGIN', __FILE__ );
+if ( ! defined( 'JWR_DEV_PLUGIN_PATH' ) ) {
+	// Plugin path constant.
+	define( 'JWR_DEV_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
+}
+if ( ! defined( 'JWR_DEV_PLUGIN_URL' ) ) {
+	// Plugin URL constant.
+	define( 'JWR_DEV_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 }
 
 require_once __DIR__ . '/helpers/plugin-updates.php';
 
-// Classes.
+// Admin pages.
 require_once __DIR__ . '/classes/class-admin-page.php';
 require_once __DIR__ . '/classes/class-admin-subpage.php';
-
 Admin_Page::init();
+
+// AME Styler.
+require_once __DIR__ . '/classes/class-ame-styler.php';
+AME_Styler::init();
